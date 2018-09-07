ccc69c2b5e2a0d0654121c6725f32dc7fc478e3b
nickborbe <nickborbe@gmail.com>
2018-09-07 15:42:45 -0400

    full crud on library app

--- week4/library-application/models/book.js
+++ week4/library-application/models/book.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema   = mongoose.Schema;
 
 const bookSchema = new Schema({
-  name: String,
+  name: {type: String, required: true},
   description: String,
   author: String,
   rating: Number
--- week4/library-application/routes/books.js
+++ week4/library-application/routes/books.js
@@ -5,20 +5,99 @@ const Book    = require('../models/book')
 
 /* GET home page */
 router.get('/books', (req, res, next) => {
-
     Book.find()
     .then((theStuffWeGetBack)=>{
         
         res.render('listOfBooks', {listOfBooks: theStuffWeGetBack})
     })
     .catch((err)=>{
-
+        next(err);
     })
 });
 
 
-router.get('/books/:theBookID', (req, res, next)=>{
 
+
+router.get('/books/new', (req, res, next)=>{
+    res.render('newBook');
+})
+
+
+router.post('/books/create', (req, res, next)=>{
+
+   const theTitle = req.body.bookTitle;
+   const theDescription = req.body.description;
+    const theAuthor = req.body.theAuthor;
+    const theRating = req.body.rating;
+
+    Book.create({
+        name: theTitle,
+        description: theDescription,
+        author: theAuthor,
+        rating: theRating
+    })
+    .then((response)=>{
+        res.redirect('/books')
+    })
+    .catch((err)=>{
+       next(err);
+    })
+})
+
+
+
+router.get('/books/edit/:bookid', (req, res, next)=>{
+    Book.findById(req.params.bookid)
+    .then((theBook)=>{
+        res.render('editBook', {book: theBook});
+    })
+    .catch((err)=>{
+        next(err);
+    })
+})
+
+router.post('/books/update/:id', (req, res, next)=>{
+   const theTitle = req.body.bookTitle;
+   const theDescription = req.body.description;
+   const theAuthor = req.body.theAuthor;
+   const theRating = req.body.rating;
+
+
+
+    Book.findByIdAndUpdate(req.params.id, {
+        name: theTitle,
+        description: theDescription,
+        author: theAuthor,
+        rating: theRating
+    })
+    .then((response)=>{
+        res.redirect('/books/'+req.params.id)
+    })
+    .catch((err)=>{
+       next(err);
+    })
+
+    console.log('body:', req.body)
+
+})
+
+
+router.post('/books/delete/:id', (req, res, next)=>{
+    Book.findByIdAndRemove(req.params.id)
+    .then((response)=>{
+        res.redirect('/books')
+    })
+    .catch((err)=>{
+       next(err);
+    })
+
+})
+
+
+
+
+
+router.get('/books/:theBookID', (req, res, next)=>{
     Book.findById(req.params.theBookID)
     .then((theActualBook)=>{
 
@@ -27,12 +106,12 @@ router.get('/books/:theBookID', (req, res, next)=>{
     })
     .catch((err)=>{
 
+        next(err)
+
     })
-    
-
-
-
 })
+
+
 
 
 
