c2605386c0590e2eb93ff609aeb3778eefddf609
nickborbe <nickborbe@gmail.com>
2018-09-10 10:27:52 -0400

    adds celebs example for celebs and movies assignment

---
+++ week5/celebs-and-movies/celebs-and-movies/bin/seeds.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const Celebrity = require('../models/Celebrity');
+
+
+
+
+
+mongoose.Promise = Promise;
+mongoose
+  .connect('mongodb://localhost/celebs-and-movies', {useMongoClient: true})
+  .then(() => {
+
+
+const tomCruise = {name: "Tom Cruise", occupation: "Crazy Guy Movie Star", catchphrase: "I'm not crazy"}
+
+const angelina = {name: "Angelina Jolie", occupation: "She used to be in movies", catchphrase: "I'm angelina Jolie!"}
+
+const stormTroop = {name: "storm trooper", occupation: "missing shots", catchphrase: "oh no, i missed"}
+
+
+        Celebrity.create([tomCruise, angelina, stormTroop])
+        .then((response)=>{
+            console.log(response)
+            mongoose.connection.close();
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+
+  }).catch(err => {
+    console.error('Error connecting to mongo', err)
+  });
+
+
+
+
+
+
+
+
