575f90b2fb2800d3ea26eeee82fd2546a7bda3a8
nickborbe <nickborbe@gmail.com>
2018-09-24 09:59:57 -0400

    intro t react

---
+++ week7/react-module-day1-start/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import "./App.css";
+
+
+class App extends Component {
+  render() {
+    return (
+        <div>
+
+            <div className="App">
+                <h1 className="title"> Hello Ironhackers! </h1>
+            </div>
+
+            <div>
+                <h3>List of Animals</h3>
+                <ul>
+                    <li> Dog</li>
+                    <li>cat</li>
+                    <li>Alligator</li>
+                </ul>
+                wow
+            </div>
+            
+        </div>
+    );
+  }
+}
+
+export default App;
