77e2321eb327acb14539461c30d207776176dac5
nickborbe <nickborbe@gmail.com>
2018-10-08 09:15:57 -0400

    abstractd the map function into the class

--- week7/project-management-client/src/components/ProjectList.js
+++ week7/project-management-client/src/components/ProjectList.js
@@ -28,20 +28,35 @@ class ProjectList extends Component {
     this.getAllProjects();
   }
 
+
+ showAllTheProjects = () => {
+
+  if(this.state.listOfProjects.length > 0){
+    
+    return (
+
+      this.state.listOfProjects.map((project, index) => {
+        return (
+          <div key={project._id}>
+        <Link to={`/projects/${project._id}`}>
+          <h3>{project.title}</h3>
+        </Link>
+        <p style={{maxWidth: '400px'}} >{project.description} </p>
+      </div>
+    )})
+
+    )
+    
+  }
+
+ }
+
+
   render(){
     return(
       <div>
         <div style={{width: '60%', float:"left"}}>
-          { this.state.listOfProjects.map((project, index) => {
-            return (
-              <div key={project._id}>
-                <Link to={`/projects/${project._id}`}>
-                  <h3>{project.title}</h3>
-                </Link>
-                <p style={{maxWidth: '400px'}} >{project.description} </p>
-              </div>
-            )})
-          }
+      {this.showAllTheProjects()}
         </div>
         <div style={{width: '40%', float:"right"}}>
             <AddProject getData={() => this.getAllProjects()}/>
